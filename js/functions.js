807bf0b5a01a98dffe87c00de916664dd96f3e1e
EJ Perez <idyey04@gmail.com>
2017-07-04 22:20:53 +0800

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,39 @@
+function generateRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getBrowserWidth() {
+    if (self.innerWidth) {
+        return self.innerWidth;
+    }
+
+    if (document.documentElement && document.documentElement.clientWidth) {
+        return document.documentElement.clientWidth;
+    }
+
+    if (document.body) {
+        return document.body.clientWidth;
+    }
+}
+
+function getBrowserHeight() {
+    if (self.innerHeight) {
+        return self.innerHeight;
+    }
+
+    if (document.documentElement && document.documentElement.clientHeight) {
+        return document.documentElement.clientHeight;
+    }
+
+    if (document.body) {
+        return document.body.clientHeight;
+    }
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
